446e29397ba27a807e8241e6380cba3db15a3d1f
CuteDachshund <gelsemium.g.elegans.756@gmail.com>
2023-05-17 22:35:59 +0900

    更新

--- lecture/phys1func.jl
+++ lecture/phys1func.jl
@@ -40,23 +40,23 @@ function gen_circularanim(data1;xvel=false,yvel=false,trj=false,ang=false,vel=fa
             end
         end
         if trj==true
-            plt=plot!(xs[1:i],ys[1:i],label="t=$(ts[i])",legend = :bottomleft,foreground_color_legend = :white,alpha=0.2,linewidth=5) # 軌道のプロット
+            plt=plot!(xs[1:i],ys[1:i],label="t=$(ts[i])",legend = :bottomleft,foreground_color_legend = :white,alpha=0.2,linewidth=3) # 軌道のプロット
         end
         if vel==true
-            plt=plot!([xs[i],xs[i]+xvs[i]], [ys[i],ys[i]+yvs[i]], arrow = arrow(),label="v",linewidth=5) # 速度のプロット
+            plt=plot!([xs[i],xs[i]+xvs[i]], [ys[i],ys[i]+yvs[i]], arrow = arrow(),label="v",linewidth=3) # 速度のプロット
         end
         if xvel==true
-            plt=plot!([xs[i],xs[i]], [ys[i],ys[i]+yvs[i]], arrow = arrow(),label="vy",linewidth=5) # 速度のx成分のプロット
+            plt=plot!([xs[i],xs[i]], [ys[i],ys[i]+yvs[i]], arrow = arrow(),label="vy",linewidth=3) # 速度のx成分のプロット
         end
         if yvel==true
-            plt=plot!([xs[i],xs[i]+xvs[i]], [ys[i],ys[i]], arrow = arrow(),label="vx",linewidth=5) # 速度のy成分のプロット
+            plt=plot!([xs[i],xs[i]+xvs[i]], [ys[i],ys[i]], arrow = arrow(),label="vx",linewidth=3) # 速度のy成分のプロット
         end
         if ang==true
-            plt=plot!([0,xs[i]],[0,ys[i]],label="",linecolor=:blue,alpha=0.2,linewidth=5) # 回転中心と質点を結ぶ直線
-            plt=plot!([0,R],[0,0],label="",linecolor=:blue ,alpha=0.2,linewidth=5) # 回転中心と質点を結ぶ直線
+            plt=plot!([0,xs[i]],[0,ys[i]],label="",linecolor=:blue,alpha=0.2,linewidth=3) # 回転中心と質点を結ぶ直線
+            plt=plot!([0,R],[0,0],label="",linecolor=:blue ,alpha=0.2,linewidth=3) # 回転中心と質点を結ぶ直線
         end
         if accs==true
-            plt=plot!([xs[i],xs[i]+xas[i]], [ys[i],ys[i]+yas[i]], arrow = arrow(),label="a",linewidth=5)
+            plt=plot!([xs[i],xs[i]+xas[i]], [ys[i],ys[i]+yas[i]], arrow = arrow(),label="a",linewidth=3)
         end
         frame(anim,plt)
     end
@@ -138,4 +138,59 @@ function phys1plot2()
     theta = range(0,2π,step=0.01)
     plot!(50*cos.(theta),50*sin.(theta),linecolor=:blue,label="",alpha=0.2,linewidth=5)
     plot!([data1[4][1],data1[4][20]], [data1[5][1],data1[5][20]], arrow = arrow(),label="",linecolor=:black,linewidth=5)
+end
+
+function gen_circularanim(data1,data2;xvel=false,yvel=false,trj=false,ang=false,vel=false,accs=false,strobe=false) # 円運動の動画を作る
+    ts = data1[1]
+    xs = data1[2]
+    ys = data1[3]
+    xvs=data1[4]
+    yvs=data1[5]
+    xas=data1[6]
+    yas=data1[7]
+    R = xs[1]
+    ts2 = data2[1]
+    xs2 = data2[2]
+    ys2 = data2[3]
+    xvs2=data2[4]
+    yvs2=data2[5]
+    xas2=data2[6]
+    yas2=data2[7]
+    R2= xs2[1]
+    theta = range(0,2π,step=0.01)#　質点描画
+    r = 5 # 質点の半径
+    anim = Animation()
+    for i in 1:length(ts)
+        plt=plot(xs[i] .+r*cos.(theta),ys[i].+r*sin.(theta),aspectratio=1,xlims=(-100,100),ylims=(-100,100),label="",xlabel="x",ylabel="y",linecolor=:blue,linewidth=3) # 質点のプロット
+        plt=plot!(xs2[i] .+r*cos.(theta),ys2[i].+r*sin.(theta),aspectratio=1,xlims=(-100,100),ylims=(-100,100),label="",xlabel="x",ylabel="y",linecolor=:blue,linewidth=3) # 質点のプロット
+        if strobe == true
+            for j in 1:i 
+                plt=plot!(xs[j] .+r*cos.(theta),ys[j].+r*sin.(theta),aspectratio=1,xlims=(-100,100),ylims=(-100,100),label="",xlabel="x",ylabel="y",linecolor=:blue,linewidth=3)
+                plt=plot!(xs2[j] .+r*cos.(theta),ys2[j].+r*sin.(theta),aspectratio=1,xlims=(-100,100),ylims=(-100,100),label="",xlabel="x",ylabel="y",linecolor=:blue,linewidth=3)
+            end
+        end
+        if trj==true
+            plt=plot!(xs[1:i],ys[1:i],label="t=$(ts[i])",legend = :bottomleft,foreground_color_legend = :white,alpha=0.2,linewidth=3) # 軌道のプロット
+        end
+        if vel==true
+            plt=plot!([xs[i],xs[i]+xvs[i]], [ys[i],ys[i]+yvs[i]], arrow = arrow(),label="v",linewidth=3) # 速度のプロット
+        end
+        if xvel==true
+            plt=plot!([xs[i],xs[i]], [ys[i],ys[i]+yvs[i]], arrow = arrow(),label="vy",linewidth=3) # 速度のx成分のプロット
+        end
+        if yvel==true
+            plt=plot!([xs[i],xs[i]+xvs[i]], [ys[i],ys[i]], arrow = arrow(),label="vx",linewidth=3) # 速度のy成分のプロット
+        end
+        if ang==true
+            plt=plot!([0,xs[i]],[0,ys[i]],label="",linecolor=:blue,alpha=0.2,linewidth=3) # 回転中心と質点を結ぶ直線
+            plt=plot!([0,R],[0,0],label="",linecolor=:blue ,alpha=0.2,linewidth=3) # 回転中心と質点を結ぶ直線
+            plt=plot!([0,xs2[i]],[0,ys2[i]],label="",linecolor=:blue,alpha=0.2,linewidth=3) # 回転中心と質点を結ぶ直線
+            plt=plot!([0,R2],[0,0],label="",linecolor=:blue ,alpha=0.2,linewidth=3) # 回転中心と質点を結ぶ直線
+        end
+        if accs==true
+            plt=plot!([xs[i],xs[i]+xas[i]], [ys[i],ys[i]+yas[i]], arrow = arrow(),label="a",linewidth=3)
+        end
+        frame(anim,plt)
+    end
+    gif(anim,fps=20)
 end
